56574a6563aa74e8ea7389b2a809f5ed70d29386
Up210206a <up210206@alumnos.upa.edu.mx>
2022-09-12 10:44:53 -0500

    modificando

---
+++ U1/01_hola.cpp
@@ -0,0 +1,25 @@
+/*
+Date: 12/09/2022
+Author: Jorge Santacruz
+Descrpiption: Mi primer programa en cpp
+Email:
+Last Modification:
+
+*/
+
+//-------------------PREPROCESSOR DIRECTIVES----------------------------
+
+//Input/Output library between the screen and the keyboard
+#include <iostream>
+
+//---------------------MAIN FUNCTION-----------------------------------
+
+int main(){
+
+    //prints a text on the screen 
+    std::cout<<"Hello World! This is not my first program";
+
+
+    //Return 0 means that the program ends succesfully 
+    return 0;
+}
