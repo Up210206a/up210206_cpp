7f20871b11a1275a4a55af075c2e11b12813b998
UP210694 <up210694@alumnos.upa.edu.mx>
2022-10-05 10:18:39 -0500

    agregando archivos

---
+++ U2/06_Productos.cpp
@@ -0,0 +1,47 @@
+/* Unit 2. Practice 6
+   Autor: Jorge Emilio Santacruz Morales
+   Fecha: 05/10/2022
+   
+*/
+
+#include <iostream>
+using namespace std;
+
+
+
+int main (){
+
+    int op=0;
+    float prods,price,acum,tot;
+
+    cout << "INTRODUCE UR OPTION" << endl;
+    cout << "1 to continue, 2 for end" << endl;
+    cin >> op;
+
+
+
+    if (op==1){
+        do{
+
+            cout << "INTRODUCE THE NUMBER OF PRODUCTS" << endl;
+            cin >> prods;
+            cout << "INTRODUCE THE PRICE OF THE PRODUCT" << endl;
+            cin >> price;
+
+            tot  += price*prods;
+
+            cout << "THE TOTAL IS: "<<  tot << endl;
+
+            cout << "CONTINUE?" << endl;
+            cin >> op;
+
+    }while (op!=2);
+
+    }
+
+
+
+
+
+    
+}
